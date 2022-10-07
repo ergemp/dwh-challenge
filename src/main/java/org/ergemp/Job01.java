2a01ef40a80eec83678bd97d60c8a9e94583f859
ergemp <ergemp@gmail.com>
2022-10-07 11:05:14 +0300

    .

--- src/main/java/org/ergemp/Job01.java
+++ src/main/java/org/ergemp/Job01.java
@@ -9,6 +9,8 @@ import org.apache.spark.sql.SparkSession;
 
 import java.util.Properties;
 
+import static org.apache.spark.sql.functions.*;
+
 public class Job01 {
     public static void main(String[] args) {
 
@@ -23,19 +25,130 @@ public class Job01 {
                 .getOrCreate();
 
         //open the local csv file
-        String csvPath = "resources/data/products.csv";
+        String productsPath = "resources/data/products.csv";
+        String ordersPath = "resources/data/orders.csv";
+        String orderItemsPath = "resources/data/order_items.csv";
+        String customersPath = "resources/data/customers.csv";
+        String paymentsPath = "resources/data/payments.csv";
+        String categoryNamePath = "resources/data/product_category_name.csv";
 
-        Dataset<Row> productsDf = spark.read().option("header",true).csv(csvPath);
+        Dataset<Row> productsDf = spark.read().option("header",true).csv(productsPath);
+        Dataset<Row> ordersDf = spark.read().option("header",true).csv(ordersPath);
+        Dataset<Row> orderItemsDf = spark.read().option("header",true).csv(orderItemsPath);
+        Dataset<Row> customersDf = spark.read().option("header",true).csv(customersPath);
+        Dataset<Row> paymentsDf = spark.read().option("header",true).csv(paymentsPath);
+        Dataset<Row> categoryDf = spark.read().option("header",true).csv(categoryNamePath);
 
         productsDf.printSchema();
+        ordersDf.printSchema();
+        orderItemsDf.printSchema();
+        customersDf.printSchema();
+        paymentsDf.printSchema();
+        categoryDf.printSchema();
+
+        //creating location_dim
+        customersDf
+                .withColumn("location_id", md5(expr("customer_city || customer_state || customer_zip_code_prefix"))).as("location_id")
+                .select(
+                        col("location_id"),
+                        col("customer_city").as("location_city"),
+                        col("customer_state").as("location_state"),
+                        col("customer_zip_code_prefix").as("location_zip_code_prefix"))
+                .distinct().show(false);
+
+        //creating customers_dim
+        customersDf
+                .withColumn("location_id", md5(expr("customer_city || customer_state || customer_zip_code_prefix"))).as("location_id")
+                .select(
+                        col("location_id"),
+                        col("customer_id"),
+                        col("customer_unique_id"))
+                .show(false);
+
+        //creating orders_dim
+        ordersDf
+                .select(col("order_id"),
+                        col("index"),
+                        col("customer_id"),
+                        col("order_status"),
+                        col("order_purchase_timestamp"),
+                        col("order_approved_at"),
+                        col("order_delivered_carrier_date"),
+                        col("order_delivered_customer_date"),
+                        col("order_estimated_delivery_date"))
+                .show(false);
+
+        //creating payments_dim
+        paymentsDf
+                .select(col("order_id"),
+                        col("payment_sequential"),
+                        col("payment_type"),
+                        col("payment_installments"),
+                        col("payment_value"))
+                .show(false);
+
+        //creating products_dim
+        productsDf
+                .select(
+                        col("product_id"),
+                        col("product_category_name"),
+                        col("product_name_lenght").as("product_name_length"),
+                        col("product_description_lenght").as("product_name_length"),
+                        col("product_photos_qty"),
+                        col("product_weight_g"),
+                        col("product_length_cm"),
+                        col("product_height_cm"),
+                        col("product_width_cm"))
+                .show(false);
+
+        //creating category_dim
+        categoryDf
+                .select(
+                        col("product_category_name").alias("category_name"),
+                        col("product_category_name_english").alias("category_name_english"))
+                .show(false);
+
+
+        //creating date_dim
+        spark.sql(" select " +
+                        " ddate, " +
+                        " year(ddate) as year," +
+                        " month(ddate) as month, " +
+                        " day(ddate) as day, " +
+                        " dayofweek(ddate) as weekday, " +
+                        " dayofyear(ddate) as yearday, " +
+                        " weekofyear(ddate) as week, " +
+                        " quarter(ddate) as quarter " +
+                        " from " +
+                        " ( " +
+                        " select " +
+                        " explode(sequence(to_date('2000-01-01'), to_date('2030-01-01'), interval 1 day)) as ddate" +
+                        " ) ")
+                .show(100,false);
+
+
+        //creating order_items_fact
+        orderItemsDf
+                .join(ordersDf,orderItemsDf.col("order_id").equalTo(ordersDf.col("order_id")),"left")
+                .join(customersDf,ordersDf.col("customer_id").equalTo(customersDf.col("customer_id")),"left")
+                .select(orderItemsDf.col("order_id"),
+                        orderItemsDf.col("order_item_id"),
+                        orderItemsDf.col("product_id"),
+                        orderItemsDf.col("seller_id"),
+                        md5(expr("customer_city || customer_state || customer_zip_code_prefix")).as("location_id"),
+                        orderItemsDf.col("shipping_limit_date"),
+                        orderItemsDf.col("price"),
+                        orderItemsDf.col("freight_value"))
+                .show(false);
+        ;
+
 
         Properties cnnProps = new Properties();
         cnnProps.setProperty("driver", "org.postgresql.Driver");
         cnnProps.setProperty("user", "postgres");
         cnnProps.setProperty("password", "postgres");
 
-        productsDf.write().mode(SaveMode.Overwrite).jdbc("jdbc:postgresql://localhost/postgres","products", cnnProps);
-
+        //productsDf.write().mode(SaveMode.Overwrite).jdbc("jdbc:postgresql://localhost/postgres","products", cnnProps);
 
     }
 }
